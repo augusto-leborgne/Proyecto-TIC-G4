233cec58a3abeec45b909b286084b976aa278ab7
augusto-leborgne <leborgneaugusto@gmail.com>
2024-08-29 12:45:19 -0300

    Initial commit

---
+++ src/main/java/org/example/proyectoticg4/ProyectoTicG4Application.java
@@ -0,0 +1,13 @@
+package org.example.proyectoticg4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTicG4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoTicG4Application.class, args);
+	}
+
+}
