65c0fddd892858d669e5fe555147c5d476592165
augusto-leborgne <leborgneaugusto@gmail.com>
2024-09-05 12:04:29 -0300

    DBD

--- src/main/java/org/example/proyectoticg4/ProyectoTicG4Application.java
+++ src/main/java/org/example/proyectoticg4/ProyectoTicG4Application.java
@@ -17,34 +17,5 @@ public class ProyectoTicG4Application {
 
 	// Get a database connection
 	Connection connection = DatabaseConnection.getConnection();
-
-	// Use the connection to execute SQL queries and interact with the database
-	try {
-		System.out.println("Conexión establecida.");
-		String sql = "CREATE TABLE IF NOT EXISTS empleados ("
-				+ "id INT PRIMARY KEY AUTO_INCREMENT, "
-				+ "nombre VARCHAR(100) NOT NULL, "
-				+ "apellido VARCHAR(100) NOT NULL, "
-				+ "email VARCHAR(100) NOT NULL UNIQUE, "
-				+ "fecha_contratacion DATE NOT NULL"
-				+ ")";
-
-		try (Statement statement = connection.createStatement()) {
-			statement.execute(sql);
-			System.out.println("Tabla creada exitosamente.");
-		}
-
-	} catch(SQLException e) {
-		e.printStackTrace();
-
-	} finally {
-		// Close the connection when done
-		if (connection != null) {
-			try {
-				connection.close();
-			} catch (SQLException e) {
-				System.err.println("Error closing connection: " + e.getMessage());
-			}
-		}
-	}
+	
 }
