5bdb6fe7ee3879b20c5d58ce76023844679e6c6b
augusto-leborgne <leborgneaugusto@gmail.com>
2024-10-10 12:58:51 -0300

    Nueva DBD con datos .env

--- src/main/java/org/example/proyectoticg4/ProyectoTicG4Application.java
+++ src/main/java/org/example/proyectoticg4/ProyectoTicG4Application.java
@@ -2,11 +2,17 @@ package org.example.proyectoticg4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class ProyectoTicG4Application {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(ProyectoTicG4Application.class, args);
 	}
 }
